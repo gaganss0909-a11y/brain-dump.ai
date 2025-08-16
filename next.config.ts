9c5c7a5e5b71ced3748d1c64e17b713f77cfc313
Gagandeep Singh <gagan.ss0909@gmail.com>
2025-08-16 14:21:26 +0000

    i am giving you it agian because it doesn't take me to the checkout

--- next.config.ts
+++ next.config.ts
@@ -18,6 +18,9 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  env: {
+    NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
+  },
   async redirects() {
     return [
       {
